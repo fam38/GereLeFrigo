047c717b15e5e09473fc8f7a64246cf0080bdf7a
Florent <FLorent.palmier@mail.com>
2016-03-30 14:56:08 +0200

    Ajout de la creation de la base de données

--- app/src/main/java/com/example/martin/gerelefrigo/ProduitOpenHelper.java
+++ app/src/main/java/com/example/martin/gerelefrigo/ProduitOpenHelper.java
@@ -8,39 +8,46 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Florent on 30/03/2016.
  */
 public class ProduitOpenHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "ARTICLE_DB";
-
+    public static final String DATABASE_NAME = "FRIGO_DB";
     public static final int DATABASE_VERSION = 1;
 
-    public static final String ARTICLE_TABLE_NAME = "articles";
+    public static final String PRODUIT_TABLE_NAME = "Produits";
+    public static final String PRODUIT_COL_ID = "NUM_P";
+    public static final String PRODUIT_COL_REEL = "Libelle";
+    public static final String PRODUIT_COL_STOCKAGE = "Stockage";
 
-    public static final String ARTICLE_COL_NAME = "name";
+    public static final String REEL_TABLE_NAME = "ProduitsReels";
+    public static final String REEL_COL_LIBELLE = "Libelle";
+    public static final String REEL_COL_CATEGORIES = "Categories";
+    public static final String REEL_COL_MARQUE = "Marque";
 
-    private static final String TABLES_CREATE = "CREATE TABLE " + ARTICLE_TABLE_NAME +
+    public static final String STOCKAGE_TABLE_NAME = "Stockages";
+    public static final String STOCKAGE_COL_NOM = "nom";
+    public static final String STOCKAGE_COL_TYPE = "Type";
 
-            " (" + ARTICLE_COL_NAME +" TEXT);";
+    private static final String TABLES_REEL_CREATE = "CREATE TABLE " + REEL_TABLE_NAME +
+            " (" + REEL_COL_LIBELLE +" TEXT, "+ REEL_COL_CATEGORIES + " TEXT, "+ REEL_COL_MARQUE +" TEXT);";
+
+    private static final String TABLES_PRODUIT_CREATE = "CREATE TABLE " + PRODUIT_TABLE_NAME +
+            " (" + PRODUIT_COL_ID +" INTEGER, "+ PRODUIT_COL_REEL + " TEXT, "+ PRODUIT_COL_STOCKAGE +" TEXT);";
+
+    private static final String TABLES_STOCKAGE_CREATE = "CREATE TABLE " + STOCKAGE_TABLE_NAME +
+            " (" + STOCKAGE_COL_NOM +" TEXT, "+ STOCKAGE_COL_TYPE +" TEXT);";
 
     public ProduitOpenHelper(Context context) {
-
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
     }
 
     @Override
-
     public void onCreate(SQLiteDatabase db) {
-
-        db.execSQL(TABLES_CREATE);
-
+        db.execSQL(TABLES_STOCKAGE_CREATE);
+        db.execSQL(TABLES_REEL_CREATE);
+        db.execSQL(TABLES_PRODUIT_CREATE);
     }
 
     @Override
-
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         if (newVersion > oldVersion) {
-
-
         }
 
     }
